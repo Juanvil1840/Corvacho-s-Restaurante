3d7faf898ab80fb1645cc1c09974ba26ba985c3d
Juan Mendoza Márquez <mendozajuanjose348@gmail.com>
2026-08-23 18:49:57 -0500

    feat: crear página menú manteniendo el modelo de capas

--- demo/src/main/java/com/example/demo/controller/menuController.java
+++ demo/src/main/java/com/example/demo/controller/menuController.java
@@ -1,14 +1,23 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.service.ProductoService;
+
 @Controller
-public class menuController {
+public class MenuController {
+
+@Autowired
+    private ProductoService productoService;
 
     @GetMapping("/menu")
-    public String menu(){
+    public String menu(Model model){
+        model.addAttribute("productos", productoService.obtenerTodos());
         return "menu";
     }
+
 
 }
