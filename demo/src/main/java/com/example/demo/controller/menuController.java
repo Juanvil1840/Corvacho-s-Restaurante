b5371768bfe5c0fec8d1a5c6d165ea597d6ea8f7
Juan Mendoza Márquez <mendozajuanjose348@gmail.com>
2026-08-24 07:35:54 -0500

    feat: changes with db

--- demo/src/main/java/com/example/demo/controller/menuController.java
+++ demo/src/main/java/com/example/demo/controller/menuController.java
@@ -3,7 +3,6 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.service.ProductoService;
 
@@ -13,8 +12,7 @@ public class MenuController {
 @Autowired
     private ProductoService productoService;
 
-    @GetMapping("/menu")
-    public String menu(Model model){
+        public String menu(Model model){
         model.addAttribute("productos", productoService.obtenerTodos());
         return "menu";
     }
