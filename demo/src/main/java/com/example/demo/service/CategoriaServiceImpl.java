8af57e7a590f160efa8d286ecd8abb2d82f8fa0c
Daniel <danielcamiloced@gmail.com>
2026-08-30 19:54:23 -0500

    Se agregó el servicio de categoría

---
+++ demo/src/main/java/com/example/demo/service/CategoriaServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.demo.service;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entities.Categoria;
+import com.example.demo.repository.CategoriaRepository;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService{
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @Override
+    public Collection<Categoria> findAll() {
+        return categoriaRepository.findAll();
+    }
+
+    @Override
+    public Categoria findById(Integer id) {
+        return categoriaRepository.findById(id);
+    }
+}
