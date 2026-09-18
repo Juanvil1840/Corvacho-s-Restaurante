3734ae245472780405da479cf986a8673da00236
Jose Gabriel <jp244089@gmail.com>
2026-09-17 20:51:10 -0500

    Los cambios realizados fueron en el pom.xml done se agrego una dependencia spring-boot-starter-validation para usar NotBlank, Email. Estos utilizados en Cliente.java, cumpliendo la tarea de Mostrar mensajes de error en caso de fallos

--- demo/src/main/java/com/example/demo/entities/Cliente.java
+++ demo/src/main/java/com/example/demo/entities/Cliente.java
@@ -10,32 +10,48 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor 
-@NoArgsConstructor
+@NoArgsConstructor  
 @Entity  
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long clienteId;
-    @Column( length = 80, nullable = false)
+
+    @NotBlank(message = "El nombre es obligatorio") //Para que el nombre no este vacio
+    @Column(length = 80, nullable = false)
     private String nombre;
-    @Column( length = 80, nullable = false)
+
+    @NotBlank(message = "El apellido es obligatorio")
+    @Column(length = 80, nullable = false)
     private String apellido;
-    @Column( unique = true, length = 80, nullable = false) 
+
+    @NotBlank(message = "El correo es obligatorio")
+    @Email //Verificacion del formato correcto del email
+    @Column(unique = true, length = 80, nullable = false)
     private String correo;
-    @Column( length = 80, nullable = false)
+
+    @NotBlank(message = "La contrasena es obligatoria")
+    @Column(length = 80, nullable = false)
     private String contraseña;
-    @Column(length = 80)
+
+    @NotBlank(message = "El telefono es obligatorio")
     private String telefono;
+
+    @NotBlank(message = "La direccion es obligatoria")
     @Column(length = 100)
     private String direccion;
+
     @Column(nullable = false)
-    private boolean activo = true;  // Por defecto, el cliente esta activo
+    private boolean activo = true;
 
     @OneToOne(mappedBy = "cliente", cascade = CascadeType.ALL)
     private Carrito carrito;
@@ -44,13 +60,12 @@ public class Cliente {
     private List<Pedido> pedidos;
 
     public Cliente(String nombre, String apellido, String correo, String contraseña, String telefono, String direccion, boolean activo) {
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.correo = correo;
-    this.contraseña = contraseña;
-    this.telefono = telefono;
-    this.direccion = direccion;
-    this.activo = activo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.activo = activo;
     }
-    
 }
